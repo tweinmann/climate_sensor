d9ed5896314dfa6951abad84d74e60eca2621293
ferris1975 <tobi.weinmann@gmail.com>
2018-11-27 22:23:57 +0100

    cleaned up comments

--- climate_sensor.ino
+++ climate_sensor.ino
@@ -1,51 +1,59 @@
+// Libraries
 #include "ESP8266WiFi.h"
 #include "ESP8266WebServer.h"
 #include <DNSServer.h>
 #include <WiFiManager.h>
-#include "DHT.h" //DHT Bibliothek laden
+#include "DHT.h" 
 
-#define DHTPIN 4 //Der Sensor wird an PIN 2 angeschlossen    
-#define DHTTYPE DHT11    // Es handelt sich um den DHT11 Sensor
+// set D2 (= pin 4) on NodeMCU V1.0
+#define DHTPIN 4    
 
-DHT dht(DHTPIN, DHTTYPE); //Der Sensor wird ab jetzt mit „dth“ angesprochen
+// set DHT sensor Type (=11)
+#define DHTTYPE DHT11  
 
+// init DHT sensor
+DHT dht(DHTPIN, DHTTYPE); 
+
+// init web server
 ESP8266WebServer server(80);
  
 void setup() {
- 
+
+  // init serial port
   Serial.begin(9600);
 
+  // init WIFI manager (will either load config from eprom or launch setup network SSID="climate_sensor" for initial configuration)
   WiFiManager wifiManager;
   wifiManager.autoConnect("climate_sensor");
- 
+
+  // register handler for "/humidity"
   server.on("/humidity", []() {   //Define the handling function for the path
-  
     server.send(200, "text/plain", String(dht.readHumidity()));
- 
   });
  
+  // register handler for "/temperature"
   server.on("/temperature", []() {   //Define the handling function for the path
-  
     server.send(200, "text/plain", String(dht.readTemperature()));
- 
   });
 
+  // register handler for "/" 
   server.on("/", []() {   //Define the handling function for the path
-  
     server.send(200, "text/html", "<a href=\"/humidity\">humidity</a>|<a href=\"/temperature\">temperature</a>");
- 
   });
 
-  server.begin();                    //Start the server
+  // start web server
+  server.begin();                    
   Serial.println("Server listening");
- 
-  dht.begin(); //DHT11 Sensor starten
+
+  // start sensor
+  dht.begin(); 
 
 }
  
 void loop() {
-
-  server.handleClient();         //Handling of incoming requests
+  
+  //Handling of incoming requests
+  server.handleClient();       
  
 }
  
